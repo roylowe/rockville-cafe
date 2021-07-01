fb680f906eeb8d90f9ce237c612b6c0f9e388bbd
RoyLowe <roylowe96@gmail.com>
2021-07-01 17:48:04 -0400

    created modals for monthly specials

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(function(){
+
+    
+
+$('#breakfast-special').click(function(){
+
+    $('#breakfast-special-modal').modal('show');
+});
+
+$('#lunch-special').click(function(){
+
+    $('#lunch-special-modal').modal('show');
+});
+
+$('#dinner-special').click(function(){
+
+    $('#dinner-special-modal').modal('show');
+});
+})
